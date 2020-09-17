7a369457b4a91b5940cbde3a1887a9829763de96
Nayla Alejandra Garcia Mireles <171G0227@rcarbonifera.tecnm.mx>
2020-09-17 15:26:38 -0500

    Actividad 3 Agregada

---
+++ Actividad 3 Navegación entre paginas web/Contollers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Actividad_3_Navegación_entre_paginas_web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad_3_Navegación_entre_paginas_web.Contollers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            villancicosContext context = new villancicosContext();
+            var villancico = context.Villancico.OrderBy(x => x.Nombre);
+
+            return View(villancico);
+        }
+
+        public IActionResult Informacion(int? id)
+        {
+            if(id==null)
+            {
+                return RedirectToAction("Index");
+            }
+            villancicosContext context = new villancicosContext();
+            var vilancico = context.Villancico.FirstOrDefault(X => X.Id == id);
+
+            if(vilancico== null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(vilancico);
+            }
+           
+        }
+
+    }
+}
---
+++ Actividad 3 Navegación entre paginas web/Models/Villancico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad_3_Navegación_entre_paginas_web.Models
+{
+    public partial class Villancico
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Compositor { get; set; }
+        public int? Anyo { get; set; }
+        public string Letra { get; set; }
+        public string VideoUrl { get; set; }
+    }
+}
